0674721b2931def28fa7df15bb7705d143fc2465
Ryan Phillips <ryanmphill@gmail.com>
2023-11-08 13:38:16 -0600

    Added functionality to register component

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -4,28 +4,46 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { FormEvent, useRef } from "react"
 import styles from "./auth.module.css"
+import { registerUser } from "@/dataManagers/authManager"
+import { useAuthContext } from "@/context/AuthContext"
 
 const Register = () => {
     const router = useRouter()
-
+    const { fetchCurrentUserId, setToken } = useAuthContext()
     const firstName = useRef<HTMLInputElement | null>(null)
     const lastName = useRef<HTMLInputElement | null>(null)
     const username = useRef<HTMLInputElement | null>(null)
     const email = useRef<HTMLInputElement | null>(null)
     const password = useRef<HTMLInputElement | null>(null)
 
-    const handleRegister = (e: FormEvent<HTMLFormElement>) => {
+    const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
         const userData = {
-            first_name: firstName?.current?.value,
-            last_name: lastName?.current?.value,
-            username: username?.current?.value,
-            email: email?.current?.value,
-            password: password?.current?.value
+            first_name: firstName?.current?.value ?? "",
+            last_name: lastName?.current?.value ?? "",
+            username: username?.current?.value ?? "",
+            email: email?.current?.value ?? "",
+            password: password?.current?.value ?? ""
         }
         console.log(userData)
         // TODO: Post the new user data to API
+        try {
+            const res = await registerUser(userData)
+            if (res.token) {
+                setToken(res.token)
+                fetchCurrentUserId()
+                router.push("/")
+                return;
+            }
+            if (res.message && res.message.includes("Username already taken")) {
+                window.alert(res.message)
+                return;
+            }
+        } catch (err) {
+            console.error(err)
+            window.alert("Unable to register user")
+        }
     }
 
     return (
