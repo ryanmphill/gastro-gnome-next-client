bb75749b3da54bf51c16bf7365df4168be134610
Ryan Phillips <ryanmphill@gmail.com>
2023-11-14 17:07:51 -0600

    Updated follows and favorites with server actions and provided immediate user feedback with useOptimistic

--- src/components/Feed/PostInteraction/FavoriteButton.tsx
+++ src/components/Feed/PostInteraction/FavoriteButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { usePathname } from "next/navigation"
-import { MouseEvent, startTransition } from "react"
+import { MouseEvent, startTransition, useOptimistic } from "react"
 import styles from "./PostInteraction.module.css"
 import { addRecipeToFavorites, removeRecipeFromFavorites } from "@/dataManagers/userManager"
 
@@ -12,8 +12,11 @@ interface FavoriteButtonProps {
 }
 
 export const FavoriteButton = ( {currentUserId, recipeId, updateProfileFavs, usersFavs} : FavoriteButtonProps ) => {
-    // // get the current user
-    // const { currentUserId } = useAuthContext()
+    
+    const [optimisticFavs, updateOptimisticFavs] = useOptimistic(
+        usersFavs,
+        (_currentState, updatedFavs: number[]) => updatedFavs   
+        )
 
     // Get the current location
     const pathname = usePathname()
@@ -21,43 +24,16 @@ export const FavoriteButton = ( {currentUserId, recipeId, updateProfileFavs, use
     // Check if the user is viewing their profile to dynamically update the profile's list of favorites
     const viewingProfile = pathname === `/userprofile/${currentUserId}`
 
-    // // Set a state variable for the user's favorites
-    // const [usersFavs, updateUsersFavs] = useState<number[]>([])
-
-    // // Define a function to fetch the current user with their favorites embedded
-    // const fetchUserFavs = useCallback(async () => {
-    //     try {
-    //         const userData = await getCurrentUser()
-    //         const favoriteArray = userData.favorites
-    //         updateUsersFavs(favoriteArray)
-    //     } catch(err) {
-    //         console.error(err)
-    //     }
-    // },[])
-
-    // // Get the data for the current user's favorites
-    // useEffect(
-    //     () => {
-    //         fetchUserFavs()
-    //     },
-    //     [fetchUserFavs] // When this array is empty, you are observing initial component state
-    // )
-
     // Handle the click to favorite a post
     const handleFavorite = async (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
 
         startTransition(() => {
+            const optimisticCopy = [ ...usersFavs ]
+            optimisticCopy.push(recipeId)
+            updateOptimisticFavs([ ...optimisticCopy ])
             addRecipeToFavorites(recipeId)
         })
-        
-        // // POST favorite object to API ///////////////////////////////////
-        // try {
-        //     await addRecipeToFavorites(recipeId)
-        //     fetchUserFavs()
-        // } catch (err) {
-        //     console.error(err)
-        // }
     }
 
     // Handle the click to unfavorite a post
@@ -65,34 +41,17 @@ export const FavoriteButton = ( {currentUserId, recipeId, updateProfileFavs, use
         event.preventDefault()
 
         startTransition(() => {
+            const optimisticCopy = [ ...usersFavs ]
+            optimisticCopy.filter((favId) => favId != recipeId)
+            updateOptimisticFavs([ ...optimisticCopy ])
             removeRecipeFromFavorites(recipeId)
         })
-
-        // //DELETE the favorite
-        // try {
-        //     await removeRecipeFromFavorites(recipeId)
-        //     // Update the user's list of favorites state
-        //     if (viewingProfile && updateProfileFavs) { // If the user is on their profile, remove the recipe from view
-        //         updateProfileFavs()
-        //     } else { // else, change the button from 'favorited ⭐' to 'favorite ☆'
-        //         fetchUserFavs()
-        //     }
-        // } catch (err) {
-        //     console.error(err)
-        // }    
     }
 
     /*Define a variable with a value of true if the user has already favorited this post, and false 
     if they haven't favorited this post yet*/
-    const alreadyFavorited = usersFavs.some(userFav => userFav === recipeId)
+    const alreadyFavorited = optimisticFavs.some(userFav => userFav === recipeId)
 
-    // // Original click events
-    // onClick={click => handleFavorite(click)}
-    // onClick={click => handleUndoFavorite(click)}
-
-    // const addFavAction = addRecipeToFavorites.bind(null, recipeId)
-    // const removeFavAction = removeRecipeFromFavorites.bind(null, recipeId)
-    // Return the UI
     return <>
         {
             !alreadyFavorited
--- src/components/Feed/PostInteraction/FollowButton.tsx
+++ src/components/Feed/PostInteraction/FollowButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { MouseEvent, startTransition, useEffect, useState } from "react"
+import { MouseEvent, startTransition, useEffect, useOptimistic, useState } from "react"
 import followImg from "../../../../public/assets/follow.png"
 import followingImg from "../../../../public/assets/following.png"
 import styles from "./PostInteraction.module.css"
@@ -16,6 +16,17 @@ interface FollowButtonProps {
 
 export const FollowButton = ({ currentUserId, userToFollowId, usersFollows, updateProfileFollowList } 
     : FollowButtonProps) => {
+
+    const [optimisticFollows, updateOptimisticFollows] = useOptimistic(
+        usersFollows,
+        (_currentState, updatedFollows: number[]) => updatedFollows   
+        )
+    
+    useEffect(
+        () => {
+            startTransition(() => updateOptimisticFollows(usersFollows))
+        },[usersFollows, updateOptimisticFollows]
+    )
     
     // Get the current location
     const pathname = usePathname()
@@ -24,75 +35,34 @@ export const FollowButton = ({ currentUserId, userToFollowId, usersFollows, upda
     // Check if the user is viewing their profile to dynamically update the profile's list of follows
     const viewingProfile = pathname === `/userprofile/${currentUserId}`
 
-    // // Set a state varialbe for if the user is following
-    // const [alreadyFollowed, updateAlreadyFollowed] = useState<boolean>(false)
-
-    // // Use the array of follows and update the alreadyFollowed state
-    // useEffect(
-    //     () => {
-    //         const isFollowing = usersFollows.some(userFollow => userFollow === userToFollowId)
-    //         if (isFollowing) {
-    //             updateAlreadyFollowed(true)
-    //         } else {
-    //             updateAlreadyFollowed(false)
-    //         }
-    //     },
-    //     [usersFollows, userToFollowId]
-    // )
-
     // Handle the click to follow a user
     const handleFollow = async (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
         
         // POST follow object to API ///////////////////////////////////
         startTransition(() => {
+            const optimisticCopy = [ ...usersFollows ]
+            optimisticCopy.push(userToFollowId)
+            updateOptimisticFollows([ ...optimisticCopy ])
             followUser(userToFollowId)
         })
-        
-        // try {
-        //     await followUser(userToFollowId)
-        //     // Update the user's list of follows state
-        //     fetchUsersFollows()
-        //     if (viewingProfile && updateProfileFollowList) {
-        //         updateProfileFollowList()
-        //     }
-        // } catch (err) {
-        //     console.error(err)
-        //     window.alert("Something went wrong")
-        // }
     }
 
     // Handle the click to unfollow a user
     const handleUndoFollow = async (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
-        
+
         //DELETE the follow object
         startTransition(() => {
+            const optimisticCopy = [ ...usersFollows ]
+            optimisticCopy.filter((userId) => userId != userToFollowId)
+            updateOptimisticFollows([ ...optimisticCopy ])
             unFollowUser(userToFollowId)
         })
-        
-        // try {
-        //     await unFollowUser(userToFollowId)
-        //     // Update the user's list of follows state
-        //     fetchUsersFollows()
-        //     if (viewingProfile && updateProfileFollowList) {
-        //         updateProfileFollowList()
-        //     }
-        // } catch (err) { 
-        //     console.error(err)
-        //     window.alert("Something went wrong")
-        // }    
     }
-    // const followUserAction = followUser.bind(null, userToFollowId)
-    // const unFollowAction = unFollowUser.bind(null, userToFollowId)
 
-    // // Original click events
-    // onClick={click => handleFollow(click)}
-    // onClick={click => handleUndoFollow(click)}
+    const alreadyFollowed = optimisticFollows.some(userFollow => userFollow === userToFollowId)
 
-    const alreadyFollowed = usersFollows.some(userFollow => userFollow === userToFollowId)
-
-    // Return the UI
     return <>
         {
             !alreadyFollowed
