7a896dbd83d1df9be077a3049443871ed1748c8e
Ryan Phillips <ryanmphill@gmail.com>
2023-12-22 09:32:28 -0600

    Replaced 'usePathname' with 'useSelectedLayoutSegment'

--- src/components/profile/SharedHeader/ProfileDisplayTab.tsx
+++ src/components/profile/SharedHeader/ProfileDisplayTab.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { usePathname, useRouter } from "next/navigation"
+import { useRouter, useSelectedLayoutSegment } from "next/navigation"
 import styles from "../profile.module.css"
 
 interface DisplayTabProps {
@@ -9,12 +9,12 @@ interface DisplayTabProps {
 }
 
 export const ProfileDisplayTab = ({profileId, currentUserId} : DisplayTabProps) => {
-    const pathname = usePathname()
     const router = useRouter()
+    const segment = useSelectedLayoutSegment()
 
     return <>
         <div className={styles["profileDisplayTab"]}>
-            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${pathname === `/profile/${profileId}` ? styles['active'] : ''}`}
+            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${segment === '(index)' ? styles['active'] : ''}`}
                 onClick={(e) => {
                     e.preventDefault()
                     router.push(`/profile/${profileId}`, {scroll: false})
@@ -26,19 +26,19 @@ export const ProfileDisplayTab = ({profileId, currentUserId} : DisplayTabProps) 
                 }
             </button>
 
-            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${pathname.includes("favorites") ? styles['active'] : ''}`}
+            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${segment === 'favorites' ? styles['active'] : ''}`}
                 onClick={(e) => {
                     e.preventDefault()
                     router.push(`/profile/${profileId}/favorites`, {scroll: false})
                 }}>Favorites</button>
 
-            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${pathname.includes("followers") ? styles['active'] : ''}`}
+            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${segment === 'followers' ? styles['active'] : ''}`}
                 onClick={(e) => {
                     e.preventDefault()
                     router.push(`/profile/${profileId}/followers`, {scroll: false})
                 }}>Followers</button>
 
-            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${pathname.includes("following") ? styles['active'] : ''}`}
+            <button className={`${styles["profileDisplayTabLink"]} ${styles["profileDisplayTab--hoverEffect"]} ${styles["underline-effect"]} ${segment === 'following' ? styles['active'] : ''}`}
                 onClick={(e) => {
                     e.preventDefault()
                     router.push(`/profile/${profileId}/following`, {scroll: false})
