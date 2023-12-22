60c4d84071d04112f9455efc3eb6bb0b99bb7160
Ryan Phillips <ryanmphill@gmail.com>
2023-12-22 00:02:25 -0600

    Moved header and nav tags to layout. Added main and footer tags

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -7,25 +7,21 @@ import styles from "./NavBar.module.css"
 export const NavBar = async () => {
     const currentUserId = await getCurrentUserId()
     return (
-        <header>
-            <nav>
-                <ul className={styles["navbar"]}>
-                    <li id={styles["logoContainer"]}>
-                        <Link href={"/"} className={styles["link_img_wrapper"]}>
-                            <img className={styles["navbar__logo"]} src={gastroLogo.src} alt="Logo"></img>
-                        </Link>
-                        <Link href={"/"} className={styles["link_title_wrapper"]}>
-                            <h2 id={styles["gastroTitle"]}>Gastro Gnome</h2>
-                        </Link>
-                    </li>
-                    <li className={styles["navbar__item"]}>
+        <ul className={styles["navbar"]}>
+            <li id={styles["logoContainer"]}>
+                <Link href={"/"} className={styles["link_img_wrapper"]}>
+                    <img className={styles["navbar__logo"]} src={gastroLogo.src} alt="Logo"></img>
+                </Link>
+                <Link href={"/"} className={styles["link_title_wrapper"]}>
+                    <h2 id={styles["gastroTitle"]}>Gastro Gnome</h2>
+                </Link>
+            </li>
+            <li className={styles["navbar__item"]}>
 
-                    </li>
-                    <li className={`${styles["navbar__item"]} ${styles["navbar__menu"]}`}>
-                        <DropdownMenu currentUser={currentUserId} />
-                    </li>
-                </ul>
-            </nav>
-        </header>
+            </li>
+            <li className={`${styles["navbar__item"]} ${styles["navbar__menu"]}`}>
+                <DropdownMenu currentUser={currentUserId} />
+            </li>
+        </ul>
     )
 }
